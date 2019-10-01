f3e94b4708cee810648d8020c4179f2024db33ea
william <junho.song@columbia.edu>
2019-09-30 22:37:29 -0400

    William Song js5178 hw4

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -3,8 +3,28 @@
 #include "fms_normal.h"
 
 namespace fms::black {
+	namespace {
+		double computeD1(double f, double sigma, double k, double t) {
+			double num = log(f / k) + (t * sigma * sigma / 2);
+			double denom = sigma * sqrt(t);
+			return num / denom;
+		}
 
-    //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
+		double computeD2(double f, double sigma, double k, double t) {
+			double d1 = computeD1(f, sigma, k, t);
+			return d1 - (sigma * sqrt(t));
+		}
+	}
 
+	inline double put(double f, double sigma, double k, double t) {
+		double d1 = computeD1(f, sigma, k, t);
+		double d2 = computeD2(f, sigma, k, t);
+		return (k * fms::normal::cdf(-d2)) - (f * fms::normal::cdf(-d1));
+	}
+
+	inline double call(double f, double sigma, double k, double t) {
+		double d1 = computeD1(f, sigma, k, t);
+		double d2 = computeD2(f, sigma, k, t);
+		return (f * fms::normal::cdf(d1)) - (k * fms::normal::cdf(d2));
+	}
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -1,8 +1,64 @@
 // xll_black.cpp
+#include "fms_black.h"
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
 
-//!!! Implement XLL.BLACK.PUT(f, sigma, k, t) and XLL.BLACK.CALL(f, sigma, k, t)
-//!!! Document both function.
-//!!! Test both function. 
-//!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
-//!!! Also test call value for these parameters.
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
 
+using namespace xll;
+
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the price of the underlying.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of the underlying.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time to expiry.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the Black Scholes value of a put option.")
+	.Documentation(
+		L"This function computes the value of a put option "
+		L"using the Black Scholes formula "
+	)
+);
+double WINAPI xll_black_put(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return fms::black::put(f, sigma, k, t);
+}
+
+#ifdef _DEBUG
+
+test test_black_put([] {
+	ensure(abs(xll_black_put(100, 0.2, 100, 0.25) - 3.988) < 1e-3);
+	});
+
+#endif
+
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the price of the underlying.")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility of the underlying.")
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the time to expiry.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the Black Scholes value of a call option.")
+	.Documentation(
+		L"This function computes the value of a put option "
+		L"using the Black Scholes formula "
+	)
+);
+double WINAPI xll_black_call(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+	return fms::black::call(f, sigma, k, t);
+}
+
+#ifdef _DEBUG
+
+test test_black_call([] {
+	ensure(abs(xll_black_call(100, 0.2, 100, 0.25) - 3.988) < 1e-3);
+	});
+
+#endif
