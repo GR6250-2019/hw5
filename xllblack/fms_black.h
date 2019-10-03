b8648a9f80a7b3d3c01df8ca5313d8be838fa3e4
Brian Schriver <bjs2205@columbia.edu>
2019-10-03 19:43:38 -0400

    I (bjs2205) added functions to calculate the price of a call or put option given the forward price, volatility, strike price, and time to maturity.

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -7,4 +7,38 @@ namespace fms::black {
     //!!! Black put and call code goes here.
     //!!! Use the functions in fms_normal.h
 
+	// Call option price computation
+	// C(S_0,t) = S_0 * N(d_1) - K * N(d_2)
+	// The price of the call option is simply the return [S_0 * N(d_1)] minus the cost [K * N(d_2)]
+	// Where the return = stock price * probability function
+	// And the cost = strike price * discount to present value * probability function
+	// N(d_1) is a probability that is 0 if the stock is below the strike price but is some probability representing the stock's value if its above strike price, simply area under the normal distribution up to a z-score (z = x - u / sigma) 
+	// Where d_1 = ln(f/k) + (sigma/2)*t / (sqrt(sigma)*sqrt(t))
+	// N(d_2) is the probability the future price will be above the strike price on expiration date
+	// Where d_2 = d_1 - (sqrt(sigma)*sqrt(t))
+
+	inline double call(double f, double sigma, double k, double t) // "inline" inserts the body of the function in every place that the function is called, rather than generating code to clal the function in the one place it is defined
+	{
+		auto d1 = (log(f / k) + ((pow(sigma, 2) / 2) * t)) / (sigma * sqrt(t));
+		auto d2 = d1 - (sigma * sqrt(t));
+		auto return_value = f * fms::normal::cdf(d1);
+		auto cost_value = k * fms::normal::cdf(d2);
+		auto call_option_value = return_value - cost_value;
+
+		return  call_option_value;
+	}
+
+	// Put option price computation
+	// P(S_0,t) = K * N(-d_2) - S_0 * N(-d_1)
+	inline double put(double f, double sigma, double k, double t) // "inline" inserts the body of the function in every place that the function is called, rather than generating code to clal the function in the one place it is defined
+	{
+		auto d1 = (log(f / k) + ((pow(sigma, 2) / 2) * t)) / (sigma * sqrt(t));
+		auto d2 = d1 - (sigma * sqrt(t));
+		auto cost_value = k * fms::normal::cdf(-d2);
+		auto return_value = f * fms::normal::cdf(-d1);
+		auto put_option_value = cost_value - return_value;
+
+		return put_option_value;
+	}
+
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -1,8 +1,102 @@
-// xll_black.cpp
+// xll_black.cpp // COMPUTES THE PUT OR CALL OPTION PRICE OF A EUROPEAN OPTION
 
 //!!! Implement XLL.BLACK.PUT(f, sigma, k, t) and XLL.BLACK.CALL(f, sigma, k, t)
 //!!! Document both function.
 //!!! Test both function. 
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
+
+#include "fms_black.h" // include the correct header file (which also include the fms_normal.h file)
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
+
+using namespace xll; // groups our xll name entities
+
+// CALL OPTION PRICING FUNCTION
+
+static AddIn xai_black_call(
+	Function(XLL_DOUBLE, L"?xll_black_call", L"XLL.BLACK.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward price.") // f is the forward price
+	.Arg(XLL_DOUBLE, L"sigma", L"is theunderlying volatility.") // sigma is the underlying volatility, a standard deviation of log returns
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.") // k is the strike price
+	.Arg(XLL_DOUBLE, L"t", L"is the time to maturity.") // is the time to maturity, in years
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the price of the call option.")
+	.Documentation(
+		L"This function computes the price of a European style call option"
+	)
+);
+
+double WINAPI xll_black_call(double f, double sigma, double k, double t) // define function for computing the put option price
+{
+#pragma XLLEXPORT
+
+	double price = std::numeric_limits<double>::quiet_NaN(); // initiate the price option storage as nan
+
+	try {
+		price = fms::black::call(f, sigma, k, t); // calculate the price of the put option
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // shows as #NUM! in Excel
+	}
+
+	return price; // return the price of the put option
+}
+
+#ifdef _DEBUG
+
+test test_black_call([] {
+	double call_test = round(xll_black_call(100, 0.2, 100, 0.25) * 1000) / 1000; // set precision to 3 decimals
+	ensure(call_test == 3.988); // Ensure that the computed price of the call option is the expected value given the known data
+	});
+
+#endif
+
+// PUT OPTION PRICING FUNCTION
+
+static AddIn xai_black_put(
+	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the forward price.") // f is the forward price
+	.Arg(XLL_DOUBLE, L"sigma", L"is theunderlying volatility.") // sigma is the underlying volatility, a standard deviation of log returns
+	.Arg(XLL_DOUBLE, L"k", L"is the strike price.") // k is the strike price
+	.Arg(XLL_DOUBLE, L"t", L"is the time to maturity.") // is the time to maturity, in years
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the price of the put option.")
+	.Documentation(
+		L"This function computes the price of a European style put option"
+	)
+);
+
+double WINAPI xll_black_put(double f, double sigma, double k, double t) // define function for computing the put option price
+{
+#pragma XLLEXPORT
+	
+	double price = std::numeric_limits<double>::quiet_NaN(); // initiate the price option storage as nan
+
+	try {
+		price = fms::black::put(f, sigma, k, t); // calculate the price of the put option
+	}
+	catch (const std::exception& ex) {
+		XLL_ERROR(ex.what());
+
+		return 0; // shows as #NUM! in Excel
+	}
+
+	return price; // return the price of the put option
+}
+
+#ifdef _DEBUG
+
+test test_black_put([] {
+	double put_test = round(xll_black_put(100, 0.2, 100, 0.25) * 1000) / 1000; // set precision to 3 decimals
+	ensure(put_test == 3.988); // Ensure that the computed price of the call option is the expected value given the known data
+	});
+
+#endif
 
