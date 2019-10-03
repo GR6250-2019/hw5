17c665beb35bf382da8e06a40c839dee1e3d254b
Tianyi Zhang <zhang.4695@outlook.com>
2019-10-03 18:58:25 -0400

    HW, Tianyi Zhang

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -4,12 +4,28 @@
 
 namespace fms::black {
 
-    //!!! Black put and call code goes here.
-    //!!! Use the functions in fms_normal.h
-	inline double pdf(double x, double mu = 0, double sigma = 1)
-	{
-		auto z = (x - mu) / sigma;
+	//!!! Black put and call code goes here.
+	inline double put(double f, double t, double k, double sigma = 1) {
 
-		return fms::normal::cdf(2);
-	}
+		if (k < 0 || t < 0 || f < 0) {
+			throw std::domain_error("fms::black::put argument (k,t,f) must be positive");
+		}
+		double d2 = -1 * (((pow(sigma, 2) * t) / 2) + log(k / f)) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+
+		return k * fms::normal::cdf(-1 * d2) - f * fms::normal::cdf(-1 * d1);
+	};
+
+	inline double call(double f, double t, double k, double sigma = 1) {
+
+		if (k < 0 || t < 0 || f < 0) {
+			throw std::domain_error("fms::black::put argument (k,t,f) must be positive");
+		}
+
+		double d2 = -1 * (((pow(sigma, 2) * t) / 2) + log(k / f)) / (sigma * sqrt(t));
+		double d1 = d2 + sigma * sqrt(t);
+
+		return f * fms::normal::cdf(d1) - k * fms::normal::cdf(d2);
+	};
+	//!!! Use the functions in fms_normal.h
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -6,3 +6,79 @@
 //!!! put = 3.988 to three decimal places for f = 100, sigma = 0.2, k = 100, t = 0.25.
 //!!! Also test call value for these parameters.
 
+#include "fms_black.h"
+#include "../xll12/xll/xll.h"
+#include "../xll12/xll/shfb/entities.h"
+
+#ifndef CATEGORY
+#define CATEGORY L"XLL"
+#endif
+using namespace std;
+using namespace xll;
+
+static AddIn xai_put(
+	Function(XLL_DOUBLE, L"?xll_put", L"XLL.PUT")
+	.Arg(XLL_DOUBLE, L"f", L"is the value of current underlying asset price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the value of time to maturity")
+	.Arg(XLL_DOUBLE, L"k", L"is the value of strike price")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the standard deviation of the normal random variable. Default is 1.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the put option.")
+	.Documentation(
+		L"This function computes the put option value"
+		L"using the standard normal density "
+		MATH_(L"e" SUP_(minus_ L"z" sup2_ L"/2") L"/" radic_ L"2" pi_)
+	)
+);
+
+double WINAPI xll_put(double f, double t, double k, double sigma = 1)
+{
+#pragma XLLEXPORT
+	/*if (sigma == 0) {
+		sigma = 1;
+	}*/
+
+	return fms::black::put(f, t, k, sigma);
+}
+
+#ifdef _DEBUG
+
+test test_put([] {
+	ensure(xll_put(100, 0.25, 100, 0.2) == 3.988);
+});
+
+#endif
+
+static AddIn xai_call(
+	Function(XLL_DOUBLE, L"?xll_call", L"XLL.CALL")
+	.Arg(XLL_DOUBLE, L"f", L"is the value of current underlying asset price.")
+	.Arg(XLL_DOUBLE, L"t", L"is the value of time to maturity")
+	.Arg(XLL_DOUBLE, L"k", L"is the value of strike price")
+	.Arg(XLL_DOUBLE, L"sigma", L"is the standard deviation of the normal random variable. Default is 1.")
+	.Category(CATEGORY)
+	.FunctionHelp(L"Return the value of the call option.")
+	.Documentation(
+		L"This function computes the call option value"
+		L"using the standard normal density "
+		MATH_(L"e" SUP_(minus_ L"z" sup2_ L"/2") L"/" radic_ L"2" pi_)
+	)
+);
+
+double WINAPI xll_call(double f, double t, double k, double sigma = 1)
+{
+#pragma XLLEXPORT
+	/*if (sigma == 0) {
+		sigma = 1;
+	}*/
+
+	return fms::black::call(f, t, k, sigma);
+}
+
+
+#ifdef _DEBUG
+
+test test_call([] {
+	ensure(xll_call(100, 0.25, 100, 0.2) == 3.988);
+});
+
+#endif
