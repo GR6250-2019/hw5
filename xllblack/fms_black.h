6c5cd675fefc207a421957c4cb347cb715d0fb77
william <junho.song@columbia.edu>
2019-10-08 22:33:13 -0400

    js5178 hw5

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -5,43 +5,43 @@
 
 namespace fms::black {
 
-	// F = f exp(sigma B_t - sigma^2 t/2) is the Black model.
-	// F <= k if and only if B_t/sqrt(t) <= (sigma^2 t/2 + log (k/f))/sigma sqrt(t)
-	template<class F, class S, class K>
-	inline auto moneyness(F f, S s, K k)
-	{
-		ensure(f > 0);
-		ensure(s > 0);
-		ensure(k > 0);
+    // F = f exp(sigma B_t - sigma^2 t/2) is the Black model.
+    // F <= k if and only if B_t/sqrt(t) <= (sigma^2 t/2 + log (k/f))/sigma sqrt(t)
+    template<class F, class S, class K>
+    inline auto moneyness(F f, S s, K k)
+    {
+        ensure(f > 0);
+        ensure(s > 0);
+        ensure(k > 0);
 
-		return (s * s / 2 + log(k / f)) / s;
-	}
+        return (s * s / 2 + log(k / f)) / s;
+    }
     
-	template<class F, class S, class K, class T>
-	inline auto put(F f, S sigma, K k, T t)
-	{
+    template<class F, class S, class K, class T>
+    inline auto put(F f, S sigma, K k, T t)
+    {
         ensure(sigma > 0);
         ensure(t > 0);
 
         auto s = sigma * sqrt(t);
-		auto d2 = -moneyness(f, s, k);
-		auto d1 = d2 + s;
+        auto d2 = -moneyness(f, s, k);
+        auto d1 = d2 + s;
 
-		return k * normal::cdf(-d2) - f * normal::cdf(-d1);
-	}
+        return k * normal::cdf(-d2) - f * normal::cdf(-d1);
+    }
 
-	template<class F, class S, class K, class T>
-	inline auto call(F f, S sigma, K k, T t)
-	{
+    template<class F, class S, class K, class T>
+    inline auto call(F f, S sigma, K k, T t)
+    {
         ensure(sigma > 0);
         ensure(t > 0);
         
         auto s = sigma * sqrt(t);
-		auto d2 = -moneyness(f, s, k);
-		auto d1 = d2 + s;
+        auto d2 = -moneyness(f, s, k);
+        auto d1 = d2 + s;
 
-		return f * normal::cdf(d1) - k * normal::cdf(d2);
-	}
+        return f * normal::cdf(d1) - k * normal::cdf(d2);
+    }
 
     // Derivative of put value with respect to f.
     template<class F, class S, class K, class T>
@@ -68,15 +68,29 @@ namespace fms::black {
         auto d2 = -moneyness(f, s, k);
         auto d1 = d2 + s;
 
-        return f*normal::pdf(d1)*t;
+        return f*normal::pdf(d1)*sqrt(t);
     }
 
     // Value of sigma for a put having value p.
     template<class F, class P, class K, class T>
     inline auto put_implied_volatility(F f, P p, K k, T t)
     {
-        //!!! Put in appropriate checks, including bounds for p.
-        return 0; // !!!implement using Newton-Raphson 
+        // !!! Put in appropriate checks, including bounds for p.
+        ensure(p > 0);
+        ensure(t > 0);
+
+        // initial guess
+        double x_, x = sqrt(2 * log(f / k) / t);
+        P p_ = 0.0;
+
+        do {
+            double v_ = vega(f, x, k, t);
+            p_ = put(f, x, k, t);
+            x_ = x - (p_ - p) / v_;
+            std::swap(x_, x);
+        } while (fabs(p_ - p) > 1e-6);
+
+        return x_;
     }
 
 } // fms::black
--- xllblack/xll_black.cpp
+++ xllblack/xll_black.cpp
@@ -6,44 +6,44 @@ using namespace xll;
 using namespace fms::black;
 
 test test_moneyness([]() {
-	double f = 100;
-	double sigma = 0.2;
-	double k = 100;
-	double t = 0.25;
+    double f = 100;
+    double sigma = 0.2;
+    double k = 100;
+    double t = 0.25;
 
-	double z = moneyness(f, sigma*sqrt(t), k);
-	ensure(z == (0.2 * 0.2 * 0.25 / 2) / (0.2 * sqrt(0.25)));
-	
-	double p = put(f, sigma, k, t);
-	ensure(fabs(p - 3.987) < 0.001);
+    double z = moneyness(f, sigma*sqrt(t), k);
+    ensure(z == (0.2 * 0.2 * 0.25 / 2) / (0.2 * sqrt(0.25)));
+    
+    double p = put(f, sigma, k, t);
+    ensure(fabs(p - 3.987) < 0.001);
 
-	double c = call(f, sigma, k, t);
-	ensure(fabs(c - 3.987) < 0.001);
+    double c = call(f, sigma, k, t);
+    ensure(fabs(c - 3.987) < 0.001);
 
-	});
+    });
 
 static AddIn xai_black_put(
-	Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
-	.Arg(XLL_DOUBLE, L"f", L"is the forward.", L"100")
-	.Arg(XLL_DOUBLE, L"sigma", L"is the volatility.", L"0.2")
-	.Arg(XLL_DOUBLE, L"k", L"is the strike.", L"100")
-	.Arg(XLL_DOUBLE, L"t", L"is the time in years to expiration.", L"0.25")
-	.Category(L"XLL")
-	.FunctionHelp(L"Return Black put value.")
+    Function(XLL_DOUBLE, L"?xll_black_put", L"XLL.BLACK.PUT")
+    .Arg(XLL_DOUBLE, L"f", L"is the forward.", L"100")
+    .Arg(XLL_DOUBLE, L"sigma", L"is the volatility.", L"0.2")
+    .Arg(XLL_DOUBLE, L"k", L"is the strike.", L"100")
+    .Arg(XLL_DOUBLE, L"t", L"is the time in years to expiration.", L"0.25")
+    .Category(L"XLL")
+    .FunctionHelp(L"Return Black put value.")
 );
 double WINAPI xll_black_put(double f, double sigma, double k, double t)
 {
 #pragma XLLEXPORT
-	double result = std::numeric_limits<double>::quiet_NaN();
+    double result = std::numeric_limits<double>::quiet_NaN();
 
-	try {
-		result = put(f, sigma, k, t);
-	}
-	catch (const std::exception& ex) {
-		XLL_ERROR(ex.what());
-	}
+    try {
+        result = put(f, sigma, k, t);
+    }
+    catch (const std::exception& ex) {
+        XLL_ERROR(ex.what());
+    }
 
-	return result;
+    return result;
 }
 
 static AddIn xai_black_call(
@@ -94,8 +94,45 @@ test test_delta([]() {
     });
 
 test test_vega([]() {
-    //!!! Implement a test for vega
+    double f = 100;
+    double sigma = .2;
+    double k = 100;
+    double t = 0.25;
 
-    });
+    double p = put(f, sigma, k, t);
+
+    for (double h : {.01, .001, .0001, 0.00001, .000001}) {
+        double dp = vega(f, sigma, k, t);
+        double p_ = put(f, sigma, k, t);
+        double _p = put(f, sigma - h, k, t);
+        double dp_ = (p_ - _p) / h;
+        double volga = (p_ - 2 * p + _p) / (h * h);
+        double error_bound = volga * h / 2;
+
+        ensure(fabs(dp - dp_) <= fabs(error_bound));
+    }
+});
 
 //!!! Implement XLL.BLACK.PUT.IMPLIED(f, p, k, t) where p is the put value.
+static AddIn xai_black_put_implied(
+    Function(XLL_DOUBLE, L"?xll_black_put_implied", L"XLL.BLACK.PUT.IMPLIED")
+    .Arg(XLL_DOUBLE, L"f", L"is the forward.", L"100")
+    .Arg(XLL_DOUBLE, L"sigma", L"is the volatility.", L"0.2")
+    .Arg(XLL_DOUBLE, L"k", L"is the strike.", L"100")
+    .Arg(XLL_DOUBLE, L"t", L"is the time in years to expiration.", L"0.25")
+    .Category(L"XLL")
+    .FunctionHelp(L"Return the Black implied volatility.")
+);
+double WINAPI xll_black_put_implied(double f, double sigma, double k, double t)
+{
+#pragma XLLEXPORT
+    double result = std::numeric_limits<double>::quiet_NaN();
+    try {
+        result = put_implied_volatility(f, sigma, k, t);
+    }
+    catch (const std::exception& ex) {
+        XLL_ERROR(ex.what());
+    }
+
+    return result;
+}
