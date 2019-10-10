cea3741ea6679446bcc288700906099b4c1a1a82
scarlett332 <sf3000@columbia.edu>
2019-10-10 15:33:00 -0400

    hw5

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -68,7 +68,7 @@ namespace fms::black {
         auto d2 = -moneyness(f, s, k);
         auto d1 = d2 + s;
 
-        return f*normal::pdf(d1)*t;
+        return f*normal::pdf(d1)*sqrt(t);
     }
 
     // Value of sigma for a put having value p.
@@ -76,7 +76,25 @@ namespace fms::black {
     inline auto put_implied_volatility(F f, P p, K k, T t)
     {
         //!!! Put in appropriate checks, including bounds for p.
-        return 0; // !!!implement using Newton-Raphson 
+		ensure(t > 0);
+		ensure(p <= k);
+		ensure(p >= k - f);
+
+		//initial guess s0
+		//double s0 = (M_SQRT2PI/sqrt(t))*(p/f);
+		double s0 = 0.8;
+		double p_ = put(f, s0, k, t);
+
+		//use Newton-Raphson
+		//x = x0 - y0/f'(x0) where y0 = f(x0)
+		//function = put(f,s,k,t) - p = 0
+		while (fabs(p_ - p) > 1e-3) {
+			double deri = vega(f, s0, k, t);
+			s0 = s0 - (p_ - p) / deri;
+			p_ = put(f, s0, k, t);
+		}
+
+        return s0; // !!!implement using Newton-Raphson 
     }
 
 } // fms::black
