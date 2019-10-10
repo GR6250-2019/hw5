149832b88f0d93c640d37dacfd21df9a8cbf8ed8
rl3107 <rl3107@columbia.edu>
2019-10-10 16:12:23 -0400

    programming for finance and comp HW5 Ruiqing Liu  Uni: rl3107

--- xllblack/fms_black.h
+++ xllblack/fms_black.h
@@ -57,26 +57,42 @@ namespace fms::black {
         return -normal::cdf(-d1);
     }
 
-    // Derivative of a put or call value with respect to sigma.
-    template<class F, class S, class K, class T>
-    inline auto vega(F f, S sigma, K k, T t)
-    {
-        ensure(sigma > 0);
-        ensure(t > 0);
-        
-        auto s = sigma * sqrt(t);
-        auto d2 = -moneyness(f, s, k);
-        auto d1 = d2 + s;
 
-        return f*normal::pdf(d1)*t;
-    }
+	// Derivative of a put or call value with respect to sigma.
+	template<class F, class S, class K, class T>
+	inline auto vega(F f, S sigma, K k, T t)
+	{
+		ensure(sigma > 0);
+		ensure(t > 0);
 
-    // Value of sigma for a put having value p.
-    template<class F, class P, class K, class T>
-    inline auto put_implied_volatility(F f, P p, K k, T t)
-    {
-        //!!! Put in appropriate checks, including bounds for p.
-        return 0; // !!!implement using Newton-Raphson 
-    }
+		auto s = sigma * sqrt(t);
+		auto d2 = -moneyness(f, s, k);
+		auto d1 = d2 + s;
+
+		return f * normal::pdf(d1) * sqrt(t);
+	}
+
+	// Value of sigma for a put having value p.
+	template<class F, class P, class K, class T>
+	inline auto put_implied_volatility(F f, P p, K k, T t)
+	{
+		//!!! Put in appropriate checks, including bounds for p.
+
+		ensure(f > 0);
+		ensure(p >=0);
+		ensure(k > 0);
+		ensure(t > 0);
+
+		auto sigma = 0.3;
+		auto C_m = put(f, sigma, k, t);
+
+		do
+		{
+			sigma = sigma + (p - C_m) / vega(f, sigma, k, t);
+			C_m = put(f, sigma, k, t);
+
+		} while(fabs(C_m - p) > 0.0001);
+		return sigma; // !!!implement using Newton-Raphson 
+	}
 
 } // fms::black
